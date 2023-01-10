bd82d006630e465d082427da3a1b66f7630f3d5f
pranavsj29 <90518531+pranavsj29@users.noreply.github.com>
2023-01-10 13:27:32 +0530

    Add files via upload

---
+++ DNA.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{  //gene for color
+    public float r;
+    public float g;
+    public float b;
+    bool dead = false;
+    public float timeToDie = 0f;
+
+    SpriteRenderer sRenderer;
+    Collider2D sCollider;
+    private void OnMouseDown()
+    {
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        sRenderer.enabled = false;
+        sCollider.enabled = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+        sRenderer.color = new Color(r, g, b);
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
